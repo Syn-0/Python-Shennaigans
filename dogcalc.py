8fb4964ebb758be93a0ba944cdd7ba931a8d7de4
unknown <soulpurification001@gmail.com>
2022-09-10 19:14:54 -0400

    all need codes

---
+++ dogcalc.py
@@ -0,0 +1,5 @@
+dog_name = input("What is your dog's name? ")
+dog_current_age = input("What is your dog's current age? ")
+dog_constant = 7
+dog_true_age = int(dog_current_age) * dog_constant
+print(dog_name+"'s",'age', 'is', dog_true_age, 'in human years')
---
+++ overtherainbow.py
@@ -0,0 +1,7 @@
+first = 'somewhere'
+last = 'over the rainbow'
+print(first, last)
+holder = first
+first = last
+last = holder
+print(first, last)
